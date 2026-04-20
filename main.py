d348900459ffc1d5db65b2acb0a3c8640d1acdfc
Abderrahim Alaoui <abderrahimalaoui181@gmail.com>
2026-04-20 04:08:51 -0400

    Lancement final de l application depuis main.py

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from services.services_salle import ServiceSalle
-from models.salle import Salle
+from views.view_salle import ViewSalle
 
-service = ServiceSalle()
-
-# ajouter
-s1 = Salle("S002", "Salle bureautique", "Bureau", 10)
-ok, msg = service.ajouter_salle(s1)
-print(msg)
-
-# afficher
-for s in service.recuperer_salles():
-    print(s.afficher_infos())
-
-# rechercher
-s = service.rechercher_salle("S002")
-if s:
-    print("Trouvée :", s.afficher_infos())
-
-# modifier
-s2 = Salle("S002", "Salle bureautique modifiee", "Bureau", 15)
-ok, msg = service.modifier_salle(s2)
-print(msg)
-
-# supprimer
-service.supprimer_salle("S002")
-print("Salle supprimée")
+app = ViewSalle()
+app.mainloop()
