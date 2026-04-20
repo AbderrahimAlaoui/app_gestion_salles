a2cd06e2e307a77aeca91a9adedb30577e6a8611
Abderrahim Alaoui <abderrahimalaoui181@gmail.com>
2026-04-20 04:05:54 -0400

    Test de la couche Service dans main.py

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
-from Data.dao_salle import DataSalle
+from services.services_salle import ServiceSalle
 from models.salle import Salle
 
-dao = DataSalle()
-
-# test connexion
-conn = dao.get_connection()
-print("Connexion réussie")
-conn.close()
+service = ServiceSalle()
 
 # ajouter
-s1 = Salle("S001", "Salle reseau", "Laboratoire", 20)
-dao.insert_salle(s1)
-print("Salle ajoutée")
+s1 = Salle("S002", "Salle bureautique", "Bureau", 10)
+ok, msg = service.ajouter_salle(s1)
+print(msg)
 
-# modifier
-s1_mod = Salle("S001", "Salle reseau A", "Laboratoire", 25)
-dao.update_salle(s1_mod)
-print("Salle modifiée")
-
-# rechercher
-salle = dao.get_salle("S001")
-if salle:
-    print(salle.afficher_infos())
-
-# afficher toutes les salles
-liste = dao.get_salles()
-for s in liste:
+# afficher
+for s in service.recuperer_salles():
     print(s.afficher_infos())
 
+# rechercher
+s = service.rechercher_salle("S002")
+if s:
+    print("Trouvée :", s.afficher_infos())
+
+# modifier
+s2 = Salle("S002", "Salle bureautique modifiee", "Bureau", 15)
+ok, msg = service.modifier_salle(s2)
+print(msg)
+
 # supprimer
-dao.delete_salle("S001")
+service.supprimer_salle("S002")
 print("Salle supprimée")
